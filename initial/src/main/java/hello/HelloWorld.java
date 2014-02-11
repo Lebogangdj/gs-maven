c562f305fc1eb96c307771980c4f495bc4de02d4
CPUT Student <stud@ubuntu.(none)>
2014-02-11 08:38:58 -0800

    Added File descriptor to the file

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -1,5 +1,13 @@
 package hello;
 
+/*
+  File: HelloWorld.java
+  This is the first class we are working with in Git
+  Author: Livhuwani Lucky Rambuda
+  StudNo: 208224262
+  Date Edited: 11 February 2014
+*/
+
 public class HelloWorld {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
